8bf85902ff3023fadfa0d6d10a865e2c19887b6d
herlanderbento <herlanderbento19@gmail.com>
2021-11-17 18:45:05 +0100

    Component subscribe

--- src/views/products.tsx
+++ src/views/products.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "../components/Products/Hero";
 import { Section } from "../components/Products/Section";
+import { Subscribe } from "../components/Subscribe";
 
 export default function Products() {
   return (
     <>
       <Hero />
       <Section />
+      <Subscribe/>
     </>
   );
 }
